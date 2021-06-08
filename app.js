847877956e7a813359b19aac0ab14013c29b5528
milan.forrat <milan.forrat@gmail.com>
2021-06-08 16:54:13 +0200

    feat: dynamic progress bar

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 let eyeBalls = document.getElementsByClassName('eye-ball');
+let leftButton = document.getElementById('left-button');
+let rightButton = document.getElementById('right-button');
+let leftArm = document.querySelector('#left-arm');
+let rightArm = document.querySelector('#right-arm');
+let topButton = document.querySelector('#top-button');
+let progressOne = document.querySelector('#progress-1');
 
+
+leftButton.style.transition = "all 0.3s";
+leftArm.style.transition = "all 0.7s";
+rightButton.style.transition = "all 0.3s";
+rightArm.style.transition = "all 0.7s";
+topButton.style.transition ="all 0.7s";
+// progressOne.style.transition = "all 10s";
 
 document.onmousemove = function (){
     // clientX = la possition du curseur du clien sur x
@@ -14,3 +27,86 @@ document.onmousemove = function (){
         eyeBalls[i].style.transform = "translate(-"+x+",-"+y+")";
     }
 }
+setInterval(function(){
+    leftButton.style.boxShadow ="1px 1px 10px rgb(255, 0, 0)";
+    leftArm.style.filter = "brightness(1.2)";
+    topButton.style.filter = "brightness(1.2)";
+}, 500);
+setInterval(function(){
+    leftButton.style.boxShadow ="1px 1px 2px rgb(255, 180, 0)";
+    leftArm.style.filter = "brightness(1)";
+    topButton.style.filter = "brightness(1)";
+}, 1500);
+setInterval(function(){
+    rightButton.style.boxShadow ="1px 1px 10px rgb(255, 0, 0)";
+    rightArm.style.filter = "brightness(1.2)";
+}, 300);
+setInterval(function(){
+    rightButton.style.boxShadow ="1px 1px 2px rgb(255, 180, 0)";
+    rightArm.style.filter = "brightness(1)";
+}, 1800);
+
+// pop over from bootstrap
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+});
+
+// let reload = true;
+// function progress(reload){
+//     if(reload == true){
+//         for(let i=0; i <= 100; i++){
+//             setInterval(function(){
+//                 progressOne.style.width = i+"px";    
+//             },100); 
+//             if(i == 100) {
+//             reload = false;
+//             }
+//         }
+//     }
+//     else{
+//         for(let j=100; j >= 0; j--){
+//             setInterval(function(){
+//                 progressOne.style.width = j+"px";
+//             },100); 
+//             if(j == 0) {
+//             reload = true;
+//             }
+//         }
+//     }
+// }
+
+// progress();
+         
+            
+let progressBar = 0;
+function progressBarUp(){
+    // console.log(progressBar);
+    progressOne.style.width = progressBar+"px"; 
+    setTimeout(function(){
+        progressBar++;
+        if(progressBar < 95) {
+
+            progressBarUp();
+        }
+        else {
+            progressBarDown();
+        }
+    },200); 
+}
+function progressBarDown() {
+    // console.log(progressBar);
+    progressOne.style.width = progressBar+"px"; 
+    setTimeout(function(){
+        progressBar--;
+        if(progressBar > 0) {
+
+            progressBarDown();
+        }
+        else {
+            progressBarUp();
+        }
+    },200); 
+}
+progressBarUp();
+   
